dfcc4b94172d5ab02ea650f90c8d9f646e81c6c8
Bernd Schoolmann <mail@quexten.com>
2019-11-25 21:34:53 +0100

    Add user crud functions to database adapter interface

--- backend/src/adapters/IDatabaseAdapter.ts
+++ backend/src/adapters/IDatabaseAdapter.ts
@@ -1,5 +1,13 @@
 import { User } from '../models/User';
 
 export interface IDatabaseAdapter {
+
     getUser(id: string): Promise<User>;
+
+    createUser(user: User): Promise<User>;
+
+    updateUser(user: User): Promise<User>;
+
+    deleteUser(id: string): Promise<void>;
+
 }
