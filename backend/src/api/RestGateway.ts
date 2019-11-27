130227d771fb53e3de1367da7ee356ad3cd9719f
Marvin Gazibarić <marvin.gazibaric@rwth-aachen.de>
2019-11-27 19:42:03 +0100

    Implement basic REST gateway functionality
    The models still lack certain methods, these are going to be added
    and connected to in future. Also the api is under discussion atm.

--- backend/src/api/RestGateway.ts
+++ backend/src/api/RestGateway.ts
@@ -3,23 +3,58 @@ import { ProfileService } from '../services/ProfileService';
 
 export class RestGateway {
 
-    constructor (private profileService: ProfileService) {
-    }
+    constructor(private profileService: ProfileService) {}
 
     public getRouter(): express.Router {
-      const router = express.Router();
+        const router = express.Router();
 
-      router.get('/profile/:id', async (
-          req: express.Request,
-          response: express.Response,
-          _next: express.NextFunction) => {
-          const id = req.params.id;
-          const profile = await this.profileService.getProfile(id);
+        // create middleware for v1 api version
+        const api = express.Router({mergeParams: true});
+        router.use('/v1', api);
 
-          response.status(200).contentType('application/json').send(profile);
+        api.get('/profile/:id', async (
+            req: express.Request,
+            res: express.Response) => {
+            const id = req.params.id;
+            const profile = await this.profileService.getProfile(id);
+
+            res.status(200).contentType('application/json').send(profile);
+        });
+        api.get('/profile/:id/name', async (
+            _req: express.Request,
+            res: express.Response) => {
+            // const id = req.params.id;
+            // const profile = await this.profileService.getProfile(id);
+
+            res.status(501).end();
+            // res.status(200).contentType('text/plain').send(profile.getName());
+        });
+        api.get('/profile/:id/description', async (
+            _req: express.Request,
+            res: express.Response) => {
+            // const id = req.params.id;
+            // const profile = await this.profileService.getProfile(id);
+
+            res.status(501).end();
+            // res.status(200).contentType('text/plain').send(profile.getDescription());
         });
 
-      return router;
-    }
+        // endpoint invalid (=> 404 not found)
+        api.use(async (
+            _req: express.Request,
+            res: express.Response,
+        ) => {
+            res.status(404).end();
+        });
 
+        // catch wrong api version (currently all other than v1, => 501 not implemented)
+        router.use(async (
+            _req: express.Request,
+            res: express.Response,
+        ) => {
+            res.status(501).end();
+        });
+
+        return router;
+    }
 }
