f9036ee53774c045fc1284777480c534f0d64d19
Nik <niklas.schulte@rwth-aachen.de>
2020-01-06 10:31:29 +0100

    Add a sample offer

--- frontend/src/app/offers/offer.ts
+++ frontend/src/app/offers/offer.ts
@@ -1,5 +1,5 @@
 export class Offer {
-    constructor(public readonly name:string, public readonly loan:number, public readonly description:string){
+    constructor(public readonly name:string, public readonly subject:string, public readonly loan:number, public readonly type:string, public readonly description:string){
         
     }
 }
--- frontend/src/app/offers/offers.service.ts
+++ frontend/src/app/offers/offers.service.ts
@@ -9,6 +9,6 @@ export class OffersService {
   constructor() { }
 
   getOffers(){
-    return [new Offer("MaL0ffsadwqa",1,"MaLo beschte")]
+    return [new Offer("MaLo ist viel zu schwer!","MaLo",420,"Student","MaLo Der Prof ist mega doof und das ist auf gar keinen Fall meine Schuld. Weil ich habe mir beim Abschreiben der Hausaufgaben immer sehr viel Mühe gegeben und den ersten Klausurversuch trotzdem nicht bestanden. asdhgiaushdioa. Helft mir bitte ich will meinen Bachelor.")]
   }
 }
